3fa061adf2a6f91491f17d19e8e2bdf307caef3b
IV Pv <iskra.1@mail.ru>
2019-01-13 12:15:12 +0300

    Просто "42"

---
+++ src/ru/astraperaspera/kc/ls2/twentyFour.java
@@ -0,0 +1,12 @@
+package ru.astraperaspera.kc.ls2;
+
+public class twentyFour {
+    private static String theUltimateQuestionOfLifeTheUniverseAndEverything(byte answer) {
+        return ("The Ultimate Question of Life, the Universe, and Everything: " + answer);
+    }
+    public static void main (String[] args) {
+        byte answer = 42;
+        System.out.println(theUltimateQuestionOfLifeTheUniverseAndEverything(answer));
+    }
+}
+
