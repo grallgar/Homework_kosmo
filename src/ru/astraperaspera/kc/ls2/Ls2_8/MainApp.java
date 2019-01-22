00708393692298dc5d377d9e55b394469761705d
IV Pv <iskra.1@mail.ru>
2019-01-23 00:11:23 +0300

    Очередной этап выполнения задания 2

---
+++ src/ru/astraperaspera/kc/ls2/Ls2_8/MainApp.java
@@ -0,0 +1,8 @@
+package ru.astraperaspera.kc.ls2.Ls2_8;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Printer oneString = new Printer();
+        Printer.printString();
+    }
+}
---
+++ src/ru/astraperaspera/kc/ls2/Ls2_8/Printer.java
@@ -0,0 +1,9 @@
+package ru.astraperaspera.kc.ls2.Ls2_8;
+
+public class Printer {
+    private static String text = "Viva la Java!";
+
+    public static void printString() {
+        System.out.println(text);
+    }
+}
