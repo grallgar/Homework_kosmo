8eab9ea2f41dc0efb62f0f4ac58ed37f54e1ba20
IV Pv <iskra.1@mail.ru>
2019-02-05 01:07:00 +0300

    решено 3.3

---
+++ src/ru/astraperaspera/kc/ls3/SevenPercent.java
@@ -0,0 +1,17 @@
+package ru.astraperaspera.kc.ls3;
+
+public class SevenPercent {
+
+        public static void calculate(double crewUp[]) {
+                double[] crewAfter = new double[crewUp.length];
+                for (int i = 0; i < crewUp.length; i++) {
+                        crewAfter[i] = crewUp[i] * 0.93;
+                        System.out.printf("%d член экипажа: До взноса: %.2f , После взноса: %.2f \n", i+1, crewUp[i], crewAfter[i]);
+                }
+        }
+
+        public static void main(String[] args) {
+                double[] crewUp = new double[] {70.00, 60.00, 55.00, 65.00, 55.00, 50.00};
+                calculate(crewUp);
+        }
+}
