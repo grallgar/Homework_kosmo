16eb51144100eb60a8307c11e3b467a4d8c82b8a
IV Pv <iskra.1@mail.ru>
2019-02-05 01:09:06 +0300

    решено 3.3; вставать через пять часов...)

--- src/ru/astraperaspera/kc/ls3/SevenPercent.java
+++ src/ru/astraperaspera/kc/ls3/SevenPercent.java
@@ -3,7 +3,8 @@ package ru.astraperaspera.kc.ls3;
 public class SevenPercent {
 
         public static void calculate(double crewUp[]) {
-                double[] crewAfter = new double[crewUp.length]; //мсассивы - ссылочные, новый массив создавать обязательно
+                //массивы - ссылочные, новый массив создавать обязательно
+                double[] crewAfter = new double[crewUp.length];
                 for (int i = 0; i < crewUp.length; i++) {
                         crewAfter[i] = crewUp[i] * 0.93;
                         System.out.printf("%d член экипажа: До взноса: %.2f , После взноса: %.2f \n", i+1, crewUp[i], crewAfter[i]);
